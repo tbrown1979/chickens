a8871dc455b0875e092019f50e91041fb4f18fb7
tbrown1979 <tbrown@uni.edu>
2014-04-28 20:59:29 -0600

    A very hacky chicken picture image cycler.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
@@ -34,3 +35,6 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.locals.images = fs.readdirSync('public/images');
+console.log(app.locals.images);
---
+++ public/javascripts/local.js
@@ -0,0 +1,36 @@
+var periodical;
+var bullpen;
+var height;
+var width;
+var ajaxholder;
+var request;
+var counter;
+var newimage;
+var rotator;
+
+function cycleimage(){
+
+  if ( counter >= images.length ) { counter=0; };
+  if ( images.length <= 1 ) {
+    clearInterval( rotator );
+    return false;
+  }
+  $('#chickens').fadeOut( 'slow',
+    function() {
+      newimage = images[counter];
+    $('#chickens').attr("src", 'images/'+newimage)
+      .fadeIn('slow');
+      counter++;
+    });
+}
+
+
+$(document).ready( function() {
+  // images = '!{chickens}'
+  // images = jQuery.parseJSON( '!{images}' );
+  counter=0;
+  rotator = setInterval(
+      function() {
+        cycleimage();
+      }, 1000 );
+});
